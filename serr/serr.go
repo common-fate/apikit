692572be026ce83bac6860259082bf6950067dd3
Josh Wilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2023-06-16 14:25:32 +0800

    implementations

--- serr/serr.go
+++ serr/serr.go
@@ -20,6 +20,38 @@ func IsNotFound(err error) bool {
 	return ok
 }
 
+type UnauthorisedError struct{}
+
+// Error implements the error interface.
+func (e UnauthorisedError) Error() string {
+	return http.StatusText(http.StatusUnauthorized)
+}
+
+func Unauthorised() error {
+	return UnauthorisedError{}
+}
+
+func IsUnauthorised(err error) bool {
+	_, ok := err.(UnauthorisedError)
+	return ok
+}
+
+type ForbiddenError struct{}
+
+// Error implements the error interface.
+func (e ForbiddenError) Error() string {
+	return http.StatusText(http.StatusForbidden)
+}
+
+func Forbidden() error {
+	return ForbiddenError{}
+}
+
+func IsForbidden(err error) bool {
+	_, ok := err.(ForbiddenError)
+	return ok
+}
+
 type BadRequestError struct {
 	Msg string
 }
